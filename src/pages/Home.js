de80ed8cefd9d6ab22016c86249b78b1f3a18e94
vakho <vakho@gmail.com>
2020-11-05 21:16:41 +0400

    axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,11 @@ import React from "react";
 import axios from 'axios';
 function Home(props) {
   const token = '7e54fa3a81f3e5ce9f5e7b296e1cea49aa7307593a9631c9b034a3116886b168'
-  axios.get(`https://gorest.co.in/public-api/posts?access-token=${token}`, {}
+  axios.get(`https://gorest.co.in/public-api/posts`, {'access-token':token}
     ).then(result => {
       if (result.status === 200) {
         let data = JSON.parse(result)
+        console.log(data)
       } else {
         console.log('error')
       }
